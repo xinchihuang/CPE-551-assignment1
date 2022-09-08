c1374b3f879167005c50a30b1508c1b9e051057d
xinchi <xhuang60@stevens.edu>
2022-09-08 17:24:58 -0400

    assignment without answer

--- answer.py
+++ answer.py
@@ -9,29 +9,34 @@ def number():
        This is to review numbers and basic operations.
        """
     # Write the value 4 to the power of 5 and assign it to variable x
-    x = 4 ** 5
+    x =
     # Write the value x divided by 3 and assign it to variable y
-    y = x/3
+    y =
     return x,y
 def strings():
     """
     This is to review numbers and strings and basic operations.
     """
 
-    # Assign a string "stevens" to a variable stevens
-    stevens = "stevens"
+    # Assign a string "stevens" to a variable stevens.
 
-    # Repeat variable stevens 7 times and assign it to variable stevens_7
-    stevens_7 = stevens*7
+    stevens =
+
+    # Repeat variable stevens 7 times and assign it to variable stevens_7.
+
+    stevens_7 =
 
     # What is the length of stevens_7?
-    length = len(stevens_7)
 
-    # Concatenate variable stevens with string " is great" and assign it to variable great
-    great = stevens +" is great"
+    length =
 
-    # Replace "great" with "good" in variable m and assign it to a new variable good
-    good = great.replace("great","good")
+    # Concatenate variable stevens with string " is great" and assign it to variable great.
+
+    great =
+
+    # Replace "great" with "good" in variable m and assign it to a new variable good.
+
+    good =
 
     return stevens, stevens_7, length, great, good
 
@@ -42,20 +47,26 @@ def list_1D():
     """
     s = "\nhoboken,is,awesome,i,like,it\n"
     #Remove whitespace characters on both side and assign it to a new variable.
-    hoboken=s.strip()
+
+    hoboken =
 
     # Split variable n on a delimiter space into a list of substrings and assign it to a new variable hoboken_list.
-    hoboken_list = hoboken.split(",")
 
-    # Get the first item in the hoboken_list and assign it to a new variable hoboken_first_item
-    hoboken_first_item=hoboken_list[0]
+    hoboken_list =
 
+    # Get the first item in the hoboken_list and assign it to a new variable hoboken_first_item.
+
+    hoboken_first_item =
+
+    ####
     l=[2,3,4,1,5,6,9,10,15,12,13,-2,-6,0,0]
-    # Sort list l (make sure you use inplace sort)
-    l.sort()
 
-    # Get the 4th to 10th item in sorted list l and assign them to a new list new_l
-    new_l=l[3:10]
+    # Sort list l (make sure you use inplace sort).
+
+
+    # Get the 4th to 10th item in sorted list l and assign them to a new list new_l.
+
+    new_l =
 
     return hoboken,hoboken_list, hoboken_first_item, l, new_l
 
@@ -64,18 +75,20 @@ def list_2D():
     #   first row is [1, 4, 5]
     #   second row is [6, 10, 11]
     #   third row is [12, 17, 38]
-    A = [[1, 4, 5],
-         [6, 10, 11],
-         [12, 17, 38]]
+
+    A =
 
     # Collect the items in the last column of matrix A using list comprehension and assign it to a new variable last_column.
-    last_column = A[-1]
 
-    # Get the item at the last row and last column of A
-    a = A[-1][-1]
+    last_column =
 
-    # Get the item at row 2 and column 1 of A
-    b = A[1][0]
+    # Get the item at the last row and last column of A.
+
+    a =
+
+    # Get the item at row 2 and column 1 of A.
+
+    b =
 
 
     return A,last_column, a, b
@@ -89,33 +102,35 @@ def dictionary():
     #   "fruit" => "apple"
     #   "quantity" => 18
     #   "color" => "red"
-    fruit_dict = {"fruit":"apple","quantity":18,"color":"red"}
+    fruit_dict =
 
-    # Get the item in dictionary fruit_dict that the key "fruit" maps to
+    # Get the item in dictionary fruit_dict that the key "fruit" maps to.
 
-    f = fruit_dict["fruit"]
+    f =
 
     # Increase the quantity of f by 1
-    
-    fruit_dict["quantity"]+=1
+
+
     return fruit_dict, f
 def dictionary_nested():
     # Create a nested dictionary Grace where:
     #   "name" => {"first_name" => "Grace", "last_name" => "Hopper"} (a dictionary)
     #   "jobs" => ["scientist", "engineer"] (a list)
     #   "age" => 85
-    Grace ={
-        "name":{"first_name": "Grace", "last_name": "Hopper"} ,
-        "jobs":["scientist", "engineer"],
-        "age":85
-    }
-    # Get the "last_name" from
-    last_name = Grace["name"]["last_name"]
-    # Add "programmer" to the list of jobs Grace has
-    Grace["jobs"].append("programmer")
 
-    # Get the third job Grace has that you recently added
-    job = Grace["jobs"][2]
+    Grace =
+
+    # Get the "last_name" from "name".
+
+    last_name =
+
+    # Add "programmer" to the list of "jobs" Grace has.
+
+
+
+    # Get the third job Grace has that you recently added.
+
+    job =
 
 
     return Grace,last_name, job
